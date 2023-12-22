88946de641c9ba8aadf887b0ed10b8d4dea6b14d
freds <frederic.souchu@laposte.net>
2023-12-22 23:06:41 +0100

    working version + improved textures

--- carts/main.lua
+++ carts/main.lua
@@ -82,17 +82,12 @@ for i=32,39 do
 	_wall_by_id[fget(i)]=i
 end
 
-function draw_walls(walls,xshift,yshift)
-	for w in all(walls) do
-		local a,b=unpack(w)
-		line(a[1]-xshift,a[2]-yshift,b[1]-xshift,b[2]-yshift,11)
-	end
-end
-
+-- debug helpers
 function v_tostr(a)
 	return "["..a[1].." "..a[2].."]"
 end
 
+-- build wall boundaries from map
 function scan(walls,start,min_ij,max_ij,id,mask,n,side)
 	-- clear flag bits at tile i j
 	local function fclear(i,j)
@@ -150,6 +145,7 @@ function _init()
 	memcpy(0x0,0x6000,0x2000)
 	_map_display(0)
 	
+	-- scan map
 	local normals={
 		[1]={-1,0},
 		[2]={0,-1},
@@ -160,22 +156,22 @@ function _init()
 	local mem=0x8000
 	local mini,minj=0,0
 	local maxi,maxj=31,31
-
 	for j=minj,maxj do
 		for i=mini,maxi do
 			local s=mget(i,j)
 			local id=fget(s)
-			-- store flag in himem
+			-- store flag in himem (faster then fget/mget)
 			poke(mem,id)
 			mem+=1
 			if id==4 then
 				local i,j=i,j
+				local offset=rnd()
 				local actor={
 					pos={i+1,j+1},
 					sx=56,sy=0,radius=24,
 					update=function(self)
-						local t=time()/4
-						self.pos={i+1+0.5*cos(t),j+1+0.5*sin(t)}
+						local t=time()/4+offset
+						self.pos={i+0.5+0.5*cos(t),j+0.5+0.5*sin(t)}
 						return true
 					end
 				}
@@ -197,6 +193,7 @@ function _init()
 		end
 	end
 	
+	-- setup floor "texture"
 	for i=0,15 do
 		for j=0,15 do
 			mset(32+i,j,i+j*16)
@@ -205,7 +202,7 @@ function _init()
 end
 
 function solid(x,y)
- return fget(mget(x,y),1)
+ return fget(mget(x,y),6)
 end
 
 function solid_area(x,y,w,h)
@@ -273,7 +270,6 @@ function wallhit(posx,posy,u,v,out)
  local mapx,mapy=posx\1,posy\1
  -- initial tile
  out[mapx|mapy<<5]=0
- local sidex,sidey
  local ddx,ddy=1/u,1/v
  local mapdx,mapdy
  local distx,disty
@@ -309,14 +305,14 @@ function wallhit(posx,posy,u,v,out)
 			side=v<0 and 2 or 4
 		end		
 		local k=mapx|mapy<<5
-		if @(0x8000|k)==2 then
+		if @(0x8000|k)==0x40 then
 			local len=0
 			if side<2 then
 				len=(mapx-posx+(1-mapdx)/2)/u
 			else
 				len=(mapy-posy+(1-mapdy)/2)/v			
 			end
-			return true,mapx,mapy,side,len,(mapx\16)+2*(mapy\16)
+			return true,mapx,mapy,side,len
 		end
 		-- non solid visible tiles
 		if (mapx|mapy)&0xffe0==0 then
@@ -332,7 +328,7 @@ function rot_inv(p,a)
 end
 
 function _draw()
- cls(5)
+ cls(0)
  palt(0,false)
 
  --
@@ -340,211 +336,231 @@ function _draw()
 	local zbuf={}
 	local do_later=autoarray()
 	local cx,cy=unpack(cam.pos)
- for i=0,127 do
- 	local angle=angles[i]
- 	local u,v=sin(angle+cam.angle),cos(angle+cam.angle)
- 	
- 	--[[
-		local hit,x1,y1,leftright=lineofsight(cam.pos[1],cam.pos[2],cam.pos[1]+10*u,cam.pos[2]+10*v,10) 
-		]]
-		local hit,x1,y1,side,dist,tileid,tx=wallhit(cx,cy,u,v,tiles)
-  -- 
- 	if hit==true then
-			local h=dists[i]/dist
-  	zbuf[i]=dist
-  	local sx,mx=16,0
-  	if side<2 then
-   	tx=2*(v*dist+cy)
-   	sx+=8  
-   	--mx+=2  
-   else
-   	tx=2*(u*dist+cx)
-   end
-   
-   -- wall coords
-	  local dy=63.5-h/2   
-	  -- scale texture dv by 16
-   local dv=(2<<4)/h
-   local err=flr(dy)-dy
-   -- scale u by 16
-	 local walls=do_later[tileid]	 
-   add(walls,function(xshift,yshift) 
-		--tline(i,dy,i,63.5+h/2,(mx+(tx%2))<<4,err*dv,0,dv) 
-		--rect(x1*8,y1*8,x1*8+7,y1*8+7,2)
-		local pct,tx,ty=8*((tx/2)&0x0.ffff)
-		if(side==0) tx,ty=x1*8,y1*8+pct
-		if(side==1) tx,ty=x1*8+8,y1*8+pct
-		if(side==2) tx,ty=x1*8+pct,y1*8+8
-		if(side==4) tx,ty=x1*8+pct,y1*8
-		tx+=xshift
-		ty+=yshift
-		rectfill(i,dy,i,63.5+h/2,sget(tx,ty))
-		end)
-  end
- end 	
- local done={}	
- for k,dist in pairs(tiles) do
- 	local i,j=k&31,k\32
-	i\=16
- 	j\=16
- 	local grid_id=i+2*j
- 	if not done[grid_id] then
- 	 	done[grid_id]=true
-	 	local x0,y0=i*512-cx*32,j*512-cy*32
-	 	local verts={
-	 	 {x0,y0,u=i,v=j},
-	 	 {x0+512,y0,u=i+16-0x0.0001,v=j},
-	 	 {x0+512,y0+512,u=i+16-0x0.0001,v=j+16},
-	 	 {x0,y0+512,u=i,v=j+16}}
-	 	local clip
-		 local znear=4
-	
-	 	for i=1,4 do
-	 	 	local v=verts[i]
-	 		local p=rot_inv(v,cam.angle)
-	 		verts[i]=p
-			if(p[1]<znear) clip=true
-				local w=64/p[1] 
-				p.w=w
-				p.x=63.5+w*p[2]
-				p.y=63.5+w*16
-				-- shift to match tline additional precision
-				p.u=v.u<<4
-				p.v=v.v<<4
+ 	for i=0,127 do
+		local angle=angles[i]+cam.angle
+		local u,v=sin(angle),cos(angle)
+		
+		local hit,x1,y1,side,dist=wallhit(cx,cy,u,v,tiles)
+		-- 
+		if hit==true then
+			local h,tex_coord=dists[i]/dist
+			zbuf[i]=dist
+			if side<2 then
+				tex_coord=v*dist+cy
+			else
+				tex_coord=u*dist+cx
 			end
-			if clip then
-				-- near clipping required?
-				local res,v0={},verts[#verts]
-				local d0=v0[1]-znear
-				for i,v1 in inext,verts do
-					local side=d0>0
-					if(side) add(res,v0)
-					local d1=v1[1]-znear
-					if (d1>0)!=side then
-						-- clip!
-						local t=d0/(d0-d1)
-						local v=v_lerp(v0,v1,t)
-						-- project
-						-- z is clipped to near plane
+		
+			--
+			do_later[(x1\16)+2*(y1\16)][i]=function(xshift,yshift,out) 				
+				local pct,tx,ty=tex_coord&0x0.ffff
+				if(side==0) tx,ty=x1-0.125,y1+pct
+				if(side==1) tx,ty=x1+1,y1+pct
+				if(side==2) tx,ty=x1+pct,y1+1
+				if(side==4) tx,ty=x1+pct,y1-0.125
+				-- collect light from texture
+				out[i]=pget(8*tx+xshift,8*ty+yshift)
+				-- returns the actual draw function
+				return function(light)
+					-- wall coords
+					local dy=63.5-h/2   
+					-- scale texture dv by 16
+					local dv=64/h
+					local err=dy\1-dy
+					if light>0 then
+						poke4(0x5f38,0x001c.0404)
+					else
+						poke4(0x5f38,0x041c.0404)
+					end
+					tline(i,dy,i,63.5+h/2,((4*tex_coord))<<4,err*dv,0,dv) 
+				end
+			end
+		end
+	end 	
+
+	local done,wall_draw,floor_light={},{},{}
+	for k,dist in pairs(tiles) do
+		local i,j=k&31,k\32
+		i\=16
+		j\=16
+		local grid_id=i+2*j
+		local grid_xshift,grid_yshift=-i*128,j*128
+		if not done[grid_id] then
+			done[grid_id]=true
+			local x0,y0=i*512-cx*32,j*512-cy*32
+			local verts={
+				{x0,y0,u=i,v=j},
+				{x0+512,y0,u=i+16-0x0.0001,v=j},
+				{x0+512,y0+512,u=i+16-0x0.0001,v=j+16},
+				{x0,y0+512,u=i,v=j+16}}
+			local clip
+			local znear=4
+		
+			for i=1,4 do
+				local v=verts[i]
+				local p=rot_inv(v,cam.angle)
+				verts[i]=p
+				if(p[1]<znear) clip=true
+					local w=64/p[1] 
+					p.w=w
+					p.x=63.5+w*p[2]
+					p.y=63.5+w*16
+					-- shift to match tline additional precision
+					p.u=v.u<<4
+					p.v=v.v<<4
+				end
+				if clip then
+					-- near clipping required?
+					local res,v0={},verts[#verts]
+					local d0=v0[1]-znear
+					for i,v1 in inext,verts do
+						local side=d0>0
+						if(side) add(res,v0)
+						local d1=v1[1]-znear
+						if (d1>0)!=side then
+							-- clip!
+							local t=d0/(d0-d1)
+							local v=v_lerp(v0,v1,t)
+							-- project
+							-- z is clipped to near plane
+							
+							v.x=63.5+(v[2]<<4)
+							v.y=63.5+(16<<4)
+							v.w=16
+							add(res,v)
+						end
+						v0,d0=v1,d1
+					end
+					verts=res
+				end		
+				
+				_map_display(1)
+				poke(0x5f54,0x00,0x60)
+				cls()
+				local mask=0b00010001
+				for a in all(_lights) do
+					local grids,r,x,y=autoarray(),a.radius/8,unpack(a.pos)
+					for i=max((x-r)\16),min((x+r)\16,31) do
+						for j=max((y-r)\16),min((y+r)\16,31) do
+							add(grids[i+2*j],a)
+						end
+					end
+					for a in all(grids[grid_id]) do
+						poke(0x5f5e,mask)
+						collect_light(_walls,a.pos[1]*8,a.pos[2]*8,a.radius,grid_xshift,grid_yshift-1)
 						
-						v.x=63.5+(v[2]<<4)
-						v.y=63.5+(16<<4)
-						v.w=16
-						add(res,v)
-					end
-					v0,d0=v1,d1
-				end
-				verts=res
-			end		
-			
-			_map_display(1)
-			poke(0x5f54,0x00,0x60)
-			cls()
-			local mask=0b00010001
-			for a in all(_lights) do
-				local grids,r,x,y=autoarray(),a.radius/8,unpack(a.pos)
-				for i=max((x-r)\16),min((x+r)\16,31) do
-					for j=max((y-r)\16),min((y+r)\16,31) do
-						add(grids[i+2*j],a)
+						mask<<=1
 					end
 				end
-				for a in all(grids[grid_id]) do
-					poke(0x5f5e,mask)
-					collect_light(_walls,a.pos[1]*8,a.pos[2]*8,a.radius,-i*128,-j*128-1)
+				poke(0x5f5e,0xff)
+
+				-- kill walls
+				pal(5,0)
+				map(i*16,j*16,0,0,16,16,0x40)
+				pal()
+
+				-- collect light at wall position
+				for i,fn in pairs(do_later[grid_id]) do
+					wall_draw[i]=fn(grid_xshift,grid_yshift,floor_light)
+				end
+								
+				-- floor "texture"
+				--fillp(0xa5a5.8)
+				--rectfill(0,0,127,127,0xf0)
+				--fillp()
+
+				poke(0x5f54,0x60,0x00)
+
+				for i=1,15 do
+					pal(i,6)
+				end
+				pal(0,5)
+				palt(0,false)
+				-- draw including sprite 0
+				poke(0x5f36, 0x9)
+				-- texture
+				poke4(0x5f38,0x0020.1010)
+				mode7(verts,#verts,i+j)
+				poke(0x5f36, 0x1)
+				if(btn(5)) for i=1,10 do flip() end
+				--sspr(0,0,128,128,64*i,64*j,64,64)
+				pal()
+
 					
-					mask<<=1
+				--polyfill(verts,cam.pos,cam.angle,-16,0,1)			
+				--[[
+				local p0=verts[#verts]
+				for i=1,#verts do
+				local p1=verts[i]
+					line(
+						p0.x,p0.y,
+						p1.x,p1.y,
+							8)
+						p0=p1
 				end
+				]]
 			end
-			poke(0x5f5e,0xff)
-			-- todo: map to "kill" wall colors
-			--fillp(0x33cc.8)
-			--rectfill(0,0,127,127,0xf0)
-			--fillp()
-			poke(0x5f54,0x60,0x00)
-			
-			poke4(0x5f38,0x0020.1010)
-			for i=1,15 do
-				pal(i,1)
-			end
-			palt(0,false)
-			-- draw including sprite 0
-			poke(0x5f36, 0x9)
-			mode7(verts,#verts,i+j)
-			poke(0x5f36, 0x1)
-			if(btn(5)) for i=1,10 do flip() end
-			--sspr(0,0,128,128,64*i,64*j,64,64)
-			pal()
-
-			--_map_display(0)
-			--poke4(0x5f38,0x001c.0202)
-			for i=1,15 do
-				pal(i,2)
-			end
-			for fn in all(do_later[grid_id]) do
-			 fn(-i*128,-j*128-1)
-			 if(btn(5)) flip()
-			end
-			pal()
-			_map_display(0)
-		 		 
-   		--polyfill(verts,cam.pos,cam.angle,-16,0,1)			
-			--[[
-			local p0=verts[#verts]
-			for i=1,#verts do
-			local p1=verts[i]
-				line(
-					p0.x,p0.y,
-					p1.x,p1.y,
-						8)
-					p0=p1
-			end
-			]]
-		end
- 	--pset(mx+i,j,6)	 
- end
-
- -- actors
- palt(14,true)
- local drawables={}
- for _,a in pairs(_actors) do
-  -- visible?
-  if tiles[flr(a.pos[1])+32*flr(a.pos[2])] then
-	  local p=rot_inv({a.pos[1]-cam.pos[1],a.pos[2]-cam.pos[2]},cam.angle)
-	  if p[1]>0.1 then
-		  local w=64/p[1]
-	  	add(drawables,{
-				info=a.pos[1].." "..a.pos[2].."\nid:"..((a.pos[1]\16)+2*(a.pos[2]\16)),
-	  		x=64+w*p[2],
-	  		depth=p[1],
-	  		sx=a.sx,
-	  		key=-64/p[1]})
-	  end
-	 end
- end
- 
- sort(drawables)
- 
- for _,a in pairs(drawables) do
-  local w=-a.key
-  -- assumes all actors to be 8x8
-  local sx,dsx=a.sx,8/w
-  local x0=a.x-w/2
-  -- clip sprite start to 0
-  if(x0<0) x0,sx=0,sx-dsx*x0
-  for dx=flr(x0),min(127,flr(a.x+w/2)-1) do
-		-- get wall distance
-		local z=zbuf[dx] or 32000
-		-- is slice visible
-		if z>a.depth then
-			sspr(sx,0,1,8,dx,64-w/2,1,w)
-		end
-		sx+=dsx
+		--pset(mx+i,j,6)	 
 	end
-	--print(a.info,x0,64-w,8)
- end
 
- palt()
- print("grid:"..(cam.pos[1]\16).." "..(cam.pos[2]\16))
+	_map_display(0)
+	-- duplicate floor
+	pal(5,11)
+	pal(6,1)
+	poke(0x5f54,0x00,0x00)
+	spr(128,0,0,16,8,false,true)
+	poke(0x5f54,0x60,0x00)
+	pal()
+
+	-- draw walls
+	palt(0,false)
+	for i,fn in pairs(wall_draw) do
+		fn(floor_light[i])
+		if(btn(5)) flip()
+	end
+
+	-- actors
+	palt(14,true)
+	palt(0,false)
+	local drawables={}
+	for _,a in pairs(_actors) do
+		-- visible?
+		if tiles[flr(a.pos[1])+32*flr(a.pos[2])] then
+			local p=rot_inv({a.pos[1]-cam.pos[1],a.pos[2]-cam.pos[2]},cam.angle)
+			if p[1]>0.1 then
+				local w=64/p[1]
+				add(drawables,{
+					info=a.pos[1].." "..a.pos[2].."\nid:"..((a.pos[1]\16)+2*(a.pos[2]\16)),
+					x=64+w*p[2],
+					depth=p[1],
+					sx=a.sx,
+					key=-64/p[1]})
+			end
+		end
+	end
+ 
+	sort(drawables)
+	
+	for _,a in pairs(drawables) do
+		local w=-a.key
+		-- assumes all actors to be 8x8
+		local sx,dsx=a.sx,8/w
+		local x0=a.x-w/2
+		-- clip sprite start to 0
+		if(x0<0) x0,sx=0,sx-dsx*x0
+		for dx=flr(x0),min(127,flr(a.x+w/2)-1) do
+			-- get wall distance
+			local z=zbuf[dx] or 32000
+			-- is slice visible
+			if z>a.depth then
+				sspr(sx,0,1,8,dx,64-w/2,1,w)
+			end
+			sx+=dsx
+		end
+		--print(a.info,x0,64-w,8)
+	end
+	palt()
+	pal(11,129,1)
 end
 
 function collect_light(walls,x0,y0,r0,xshift,yshift)
@@ -582,6 +598,7 @@ function collect_light(walls,x0,y0,r0,xshift,yshift)
 	 rectfill(x,y,r0,y)
 	end	
 	camera() 
-	circfill(x0+0.5,y0+0.5,r0/12,0)
+	--light emitter shadow
+	--circfill(x0+0.5,y0+0.5,r0/12,0)
 	clip()
  end
